908734368be4c261a5c5cc502be034c254f4cf80
beccoder <bekhzodkhudoyarov@gmail.com>
2023-06-12 01:35:47 +0500

    done

---
+++ src/s21_calc/CMakeFiles/Calculator.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/alfonzoj/calc/src/Calculator/s21_smart_calc.c" "CMakeFiles/Calculator.dir/s21_smart_calc.c.o" "gcc" "CMakeFiles/Calculator.dir/s21_smart_calc.c.o.d"
+  "/Users/alfonzoj/calc/src/Calculator/s21_stack_data_structure.c" "CMakeFiles/Calculator.dir/s21_stack_data_structure.c.o" "gcc" "CMakeFiles/Calculator.dir/s21_stack_data_structure.c.o.d"
+  "/Users/alfonzoj/calc/src/s21_calc/Calculator_autogen/mocs_compilation.cpp" "CMakeFiles/Calculator.dir/Calculator_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Calculator.dir/Calculator_autogen/mocs_compilation.cpp.o.d"
+  "/Users/alfonzoj/calc/src/Calculator/calculator.cpp" "CMakeFiles/Calculator.dir/calculator.cpp.o" "gcc" "CMakeFiles/Calculator.dir/calculator.cpp.o.d"
+  "/Users/alfonzoj/calc/src/Calculator/credit.cpp" "CMakeFiles/Calculator.dir/credit.cpp.o" "gcc" "CMakeFiles/Calculator.dir/credit.cpp.o.d"
+  "/Users/alfonzoj/calc/src/Calculator/deposit.cpp" "CMakeFiles/Calculator.dir/deposit.cpp.o" "gcc" "CMakeFiles/Calculator.dir/deposit.cpp.o.d"
+  "/Users/alfonzoj/calc/src/Calculator/graph.cpp" "CMakeFiles/Calculator.dir/graph.cpp.o" "gcc" "CMakeFiles/Calculator.dir/graph.cpp.o.d"
+  "/Users/alfonzoj/calc/src/Calculator/main.cpp" "CMakeFiles/Calculator.dir/main.cpp.o" "gcc" "CMakeFiles/Calculator.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/s21_calc/CMakeFiles/Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Calculator_autogen.dir/ParseCache.txt"
+  "Calculator_autogen"
+  "CMakeFiles/Calculator.dir/Calculator_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Calculator.dir/Calculator_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Calculator.dir/calculator.cpp.o"
+  "CMakeFiles/Calculator.dir/calculator.cpp.o.d"
+  "CMakeFiles/Calculator.dir/credit.cpp.o"
+  "CMakeFiles/Calculator.dir/credit.cpp.o.d"
+  "CMakeFiles/Calculator.dir/deposit.cpp.o"
+  "CMakeFiles/Calculator.dir/deposit.cpp.o.d"
+  "CMakeFiles/Calculator.dir/graph.cpp.o"
+  "CMakeFiles/Calculator.dir/graph.cpp.o.d"
+  "CMakeFiles/Calculator.dir/main.cpp.o"
+  "CMakeFiles/Calculator.dir/main.cpp.o.d"
+  "CMakeFiles/Calculator.dir/s21_smart_calc.c.o"
+  "CMakeFiles/Calculator.dir/s21_smart_calc.c.o.d"
+  "CMakeFiles/Calculator.dir/s21_stack_data_structure.c.o"
+  "CMakeFiles/Calculator.dir/s21_stack_data_structure.c.o.d"
+  "Calculator.app/Contents/MacOS/Calculator"
+  "Calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
